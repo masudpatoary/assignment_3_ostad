6a2c63f53e54e12194309854611c337ed08fbc61
unknown <masudpatoary@gmail.com>
2024-06-24 03:12:47 +0600

    id validation in numeric char

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,24 +7,41 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     public function index($id){
+        function validateNumericString($str) {
+            for ($i = 0; $i < strlen($str); $i++) {
+                if (!is_numeric($str[$i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        // validateNumericString($id);
+        // $idInNum = (int)$id;
+        // echo $idInNum."\n"."\n";
+        // echo gettype($idInNum)."\n"."\n";
 
-        $name = "Donald Trump";
-        $age = "75";
-        $data = [
-            'id' => $id,
-            'name' => $name,
-            'age' => $age,
-        ];
+        if(validateNumericString($id)==true){
+            $name = "Donald Trump";
+            $age = "75";
+            $data = [
+                'id' => $id,
+                'name' => $name,
+                'age' => $age,
+            ];
+    
+            $cookie = cookie(
+                $name = 'access_token',
+                $value = '123-XYZ',
+                $minutes = 1,
+                $path = '/',
+                $domain = $_SERVER['SERVER_NAME'],
+                $secure = false,
+                $httpOnly = true
+            );
+            return response($data, 200)->withCookie($cookie);
+        }else{
+            return abort(404);
+        }
 
-        $cookie = cookie(
-            $name = 'access_token',
-            $value = '123-XYZ',
-            $minutes = 1,
-            $path = '/',
-            $domain = $_SERVER['SERVER_NAME'],
-            $secure = false,
-            $httpOnly = true
-        );
-        return response($data, 200)->withCookie($cookie);
     }
 }
